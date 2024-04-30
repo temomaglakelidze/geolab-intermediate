998131c04574c53c96d0bf743e72bcb45f609c30
temomaglakelidze <temomaghlakelidze37b@geolab.edu.ge>
2024-04-30 04:09:32 +0400

    Half a slideshow It was done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
-setInterval(function () {
-  var zoomableImage = document.querySelector("cover-img");
-  zoomableImage.style.transform = "scale(1.1)";
-});
+let slideIndex = 1;
+changeSlides();
+
+function addSlide(x) {
+  slideIndex += x;
+  changeSlides();
+}
+
+function deductSlide(x) {
+  slideIndex = x;
+  changeSlides();
+}
+
+function changeSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("circle");
+  let arrow = document.getElementsByClassName("arrow");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
