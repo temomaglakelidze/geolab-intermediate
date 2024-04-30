cf1e47f40ff6158b3aa59c73cd289b81fdad8841
temomaglakelidze <temomaghlakelidze37b@geolab.edu.ge>
2024-04-30 16:20:59 +0400

    slide is done, almost

--- js/app.js
+++ js/app.js
@@ -7,17 +7,35 @@ function addSlide(x) {
 }
 
 function deductSlide(x) {
-  slideIndex = x;
+  slideIndex -= x;
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
   changeSlides();
 }
 
-function changeSlides(n) {
+function changeSlides() {
   let i;
   let slides = document.getElementsByClassName("slide");
   let dots = document.getElementsByClassName("circle");
-  let arrow = document.getElementsByClassName("arrow");
+  let plus = document.getElementsByClassName("plus")[0];
+  let minus = document.getElementsByClassName("minus")[0];
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+  if (slideIndex === slides.length) {
+    plus.style.display = "none";
+  } else {
+    plus.style.display = "block";
+  }
+  if (slides.length === 1) {
+    minus.style.display = "none";
+  } else {
+    minus.style.display = "block";
+  }
   slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add("active");
 }
